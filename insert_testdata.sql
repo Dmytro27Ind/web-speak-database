82f7a6b25378e8d6249c311e53c659072ea60626
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-08-20 13:11:23 +0200

    post table added.

--- insert_testdata.sql
+++ insert_testdata.sql
@@ -1,29 +1,22 @@
 -- Active: 1660946044762@@127.0.0.1@5432@webspeak
 
 SELECT * FROM person;
-SELECT * FROM settings;
-ROLLBACK;
+SELECT * FROM language_code;
+SELECT * FROM post;
 
-BEGIN;
-INSERT INTO person (email, person_password, first_name, last_name, person_tag, birthday, sex, photo_path)
-VALUES ('dmytro27kagirov@gmail.com', 'test_password', 'Dmytro', 'Kagirov',
-        '@lyn', '2002-02-14', 'male', 'test_pass');
-INSERT INTO settings (ui_language)
-VALUES ('en');
-COMMIT;
+INSERT INTO language_code
+VALUES
+(1, 'en'),
+(2, 'sk');
 
-BEGIN;
-INSERT INTO person (email, person_password, first_name, last_name, person_tag, birthday, sex, photo_path)
-VALUES ('artem@gmail.com', 'test_password', 'Artem', 'Kagirov',
-        '@art', '2001-01-15', 'male', 'test_pass');
-INSERT INTO settings (ui_language)
-VALUES ('sk');
-COMMIT;
+INSERT INTO person (email, person_password, first_name, last_name, person_tag, birthday, sex, photo_path, fk_language_id)
+VALUES
+('dmytro27kagirov@gmail.com', 'test_password', 'Dmytro', 'Kagirov', '@lyn', '2002-02-14', 'male', 'test_path', 1),
+('artem@gmail.com', 'test_password', 'Artem', 'Kagirov', '@art', '2001-01-15', 'male', 'test_path', 2),
+('elena11@gmail.com', 'test_password', 'Elena', 'Klimova', '@elen11', '1995-02-12', 'female', 'test_path', 1);
 
-BEGIN;
-INSERT INTO person (email, person_password, first_name, last_name, person_tag, birthday, sex, photo_path)
-VALUES ('elena11@gmail.com', 'test_password', 'Elena', 'Klimova',
-        '@elen11', '1995-02-12', 'female', 'test_pass');
-INSERT INTO settings (ui_language)
-VALUES ('en');
-COMMIT;
+INSERT INTO post(fk_person_id, content, img_path)
+VALUES
+(1, 'My first post on Web-Speak', 'img_path'),
+(1, 'My second post. Hello', 'img_path'),
+(2, 'Artem. First post', 'img_path');
--- webspeak.sql
+++ webspeak.sql
@@ -1,29 +1,42 @@
 -- Active: 1660946044762@@127.0.0.1@5432@webspeak
+DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS person;
-DROP TABLE IF EXISTS settings;
+DROP TABLE IF EXISTS language_code;
+
+CREATE TABLE language_code
+(
+    language_id int         PRIMARY KEY,
+    ui_language char(2)     NOT NULL        UNIQUE
+);
 
 CREATE TABLE person
 (
-    person_id           INT GENERATED ALWAYS AS IDENTITY NOT NULL,
-    email               VARCHAR(255) NOT NULL UNIQUE,
-    person_password     VARCHAR(255) NOT NULL,
-    first_name          VARCHAR(30) NOT NULL,
-    last_name           VARCHAR(30) NOT NULL,
-    person_tag          VARCHAR(30) NOT NULL UNIQUE,
-    birthday            DATE,
-    sex                 TEXT,
-    registration_date   DATE DEFAULT NOW(),
-    photo_path          VARCHAR(255),
+    person_id           int             NOT NULL    GENERATED ALWAYS AS IDENTITY,
+    email               varchar(255)    NOT NULL    UNIQUE,
+    person_password     varchar(255)    NOT NULL,
+    first_name          varchar(30)     NOT NULL,
+    last_name           varchar(30)     NOT NULL,
+    person_tag          varchar(30)     NOT NULL    UNIQUE,
+    birthday            date,
+    sex                 text,
+    registration_date   date            DEFAULT NOW(),
+    photo_path          varchar(255),
+    fk_language_id      int,
 
     CONSTRAINT PK_person_person_id PRIMARY KEY(person_id),
-    CONSTRAINT CHK_person_sex CHECK (sex = 'male' OR sex='female')
+    CONSTRAINT CHK_person_sex CHECK (sex = 'male' OR sex='female'),
+    CONSTRAINT FK_person_language_id FOREIGN KEY(fk_language_id) REFERENCES language_code(language_id)
 );
 
-CREATE TABLE settings
+CREATE TABLE post
 (
-    fk_person_id    INT GENERATED ALWAYS AS IDENTITY NOT NULL,
-    ui_language     TEXT    NOT NULL,
+    post_id         int             NOT NULL    GENERATED ALWAYS AS IDENTITY,
+    fk_person_id    int             NOT NULL,
+    content         varchar(280),
+    img_path        varchar(255),
+    likes           int             NOT NULL    DEFAULT 0,
+    post_date       TIMESTAMPTZ     NOT NULL    DEFAULT NOW(),
 
-    CONSTRAINT FK_settings_person_id FOREIGN KEY(fk_person_id) REFERENCES person(person_id),
-    CONSTRAINT CHK_settings_ui_language CHECK (ui_language IN ('en', 'sk'))
-)
+    CONSTRAINT CHK_post_likes CHECK (likes >= 0),
+    CONSTRAINT FK_post_person_id FOREIGN KEY(fk_person_id) REFERENCES person(person_id)
+);
